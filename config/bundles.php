4898bcab293cb6a8541241b5f48256ad0268bcf2
Ilya Ukhanov <ukhanov.ilya.67@outlook.com>
2025-01-18 18:53:02 +0100

    Registration form

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,6 @@ return [
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
