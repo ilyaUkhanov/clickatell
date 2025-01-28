069def8fbd156a0cd6a2ae36877b9c19e0b4c0e9
Ilya Ukhanov <ukhanov.ilya.67@outlook.com>
2025-01-28 12:49:30 +0100

    Monolog default config

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,5 @@ return [
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\UX\Icons\UXIconsBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
