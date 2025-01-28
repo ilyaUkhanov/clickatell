6087ab9e9c3e3cb201ab5586700fbd0f323b1dd4
Ilya Ukhanov <ukhanov.ilya.67@outlook.com>
2025-01-28 12:14:00 +0100

    Add banned phones

--- src/Controller/SMSController.php
+++ src/Controller/SMSController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Banned;
+use App\Repository\BannedRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,8 +12,15 @@ use Symfony\Component\Routing\Attribute\Route;
 class SMSController extends AbstractController
 {
     #[Route('/messages/callback', name: 'app_sms')]
-    public function index(): Response
+    public function index(EntityManagerInterface $em, BannedRepository $repository): Response
     {
-        return $this->json([]);
+        $banned = new Banned();
+        $banned->setPhone("33664550920");
+        $em->persist($banned);
+        $em->flush();
+
+        $all = $repository->findAll();
+
+        return $this->json([ $all ]);
     }
 }
---
+++ src/Entity/Banned.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\BannedRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: BannedRepository::class)]
+class Banned
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $phone = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getPhone(): ?string
+    {
+        return $this->phone;
+    }
+
+    public function setPhone(string $phone): static
+    {
+        $this->phone = $phone;
+
+        return $this;
+    }
+}
